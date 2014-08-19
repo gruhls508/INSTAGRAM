76cca6fba8fd4a2a18ae05c10fd2555f8ca96b92
Patrick Hogan <phogan@beatsmusic.com>
2014-08-19 12:05:56 -0500

    Edited sign in and login vc's

--- LoginViewController.m
+++ LoginViewController.m
@@ -9,7 +9,7 @@
 #import "LoginViewController.h"
 
 @interface LoginViewController ()
-@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
 
 @end
@@ -20,18 +20,38 @@
 {
     [super viewDidLoad];
     
-    self.emailTextField.placeholder = @"Email";
-    self.emailTextField.textAlignment = NSTextAlignmentCenter;
+    self.usernameTextField.placeholder = @"Email";
+    self.usernameTextField.textAlignment = NSTextAlignmentCenter;
     
     self.passwordTextField.placeholder = @"Password";
     self.passwordTextField.textAlignment = NSTextAlignmentCenter;
 }
 
-- (IBAction)onEmailEntered:(id)sender {
+- (IBAction)onLoginPressed:(id)sender
+{
+    NSString *username = [self.usernameTextField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSString *password = [self.passwordTextField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    
+    if ([username length] ==0 || [password length] == 0)
+    {
+        UIAlertView *alertview = [[UIAlertView alloc] initWithTitle:@"Oops!" message:@"Make sure you enter a username, password" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        
+        [alertview show];
+    }
+    else {
+        [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser *user, NSError *error) {
+            if (error) {
+                UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Sorry" message:[error.userInfo objectForKey:@"error"] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                [alertView show];
+            }
+            else {
+                [self.navigationController popToRootViewControllerAnimated:YES];
+            }
+        }];
+    }
+
 }
 
-- (IBAction)onPasswordEntered:(id)sender {
-}
 
 
 
--- SignUpViewController.m
+++ SignUpViewController.m
@@ -9,6 +9,9 @@
 #import "SignUpViewController.h"
 
 @interface SignUpViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@property (weak, nonatomic) IBOutlet UITextField *emailField;
 
 @end
 
@@ -18,5 +21,42 @@
 {
     [super viewDidLoad];
 }
+
+- (IBAction)signup:(id)sender {
+    
+    NSString *username = [self.usernameField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSString *password = [self.passwordField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    NSString *email = [self.emailField.text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    
+    if ([username length] ==0 || [password length] == 0 || [email length] == 0)
+    {
+        UIAlertView *alertview = [[UIAlertView alloc] initWithTitle:@"Oops!" message:@"Make sure you enter a username, password, and email address" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        
+        [alertview show];
+    }
+    
+    else
+    {
+        PFUser *newUser = [PFUser user];
+        newUser[@"username"] = username;
+        newUser[@"password"] = password;
+        newUser[@"email"] = email;
+        
+        [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+            if (error) {
+                UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Sorry" message:[error.userInfo objectForKey:@"error"] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+                [alertView show];
+            }
+            else {
+                [self.navigationController popToRootViewControllerAnimated:YES];
+            }
+        }];
+    }
+}
+- (IBAction)onSignUp:(id)sender
+{
+    
+}
+
 
 @end
