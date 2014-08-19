04db0f96a7a5dfa45ddae2cb644f326342539060
Glen Ruhl <glen.ruhl.90@gmail.com>
2014-08-19 13:06:23 -0500

    glensCommitMessage = [insertSassyCommitMessage methodCall];

---
+++ PhotoUploadViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoUploadViewController.h
+//  INSTAGRAM
+//
+//  Created by Glen Ruhl on 8/19/14.
+//  Copyright (c) 2014 MM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoUploadViewController : UIViewController
+
+@end
---
+++ PhotoUploadViewController.m
@@ -0,0 +1,54 @@
+//
+//  PhotoUploadViewController.m
+//  INSTAGRAM
+//
+//  Created by Glen Ruhl on 8/19/14.
+//  Copyright (c) 2014 MM. All rights reserved.
+//
+
+#import "PhotoUploadViewController.h"
+
+@interface PhotoUploadViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@property UIImagePickerController *imagePicker;
+
+
+
+//  THE FOLLOWING PROPERTY NEEDS TO BE HOOKED UP TO THE IMAGE CONTAINER THAT WILL DISPLAY IN THE VIEW
+
+@property (weak, nonatomic) IBOutlet UIImageView *displayAddedImageView;
+
+
+@end
+
+@implementation PhotoUploadViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.imagePicker = [UIImagePickerController new];
+    
+    
+    self.imagePicker.delegate = self;
+    self.imagePicker.navigationController.delegate = self;
+}
+
+
++(BOOL)isSourceTypeAvailable:(UIImagePickerControllerSourceType)
+UIImagePickerControllerSourceTypePhotoLibrary
+{
+    return YES;
+}
+
+
+
+//  DON'T FORGET TO CONNECT THIS BUTTON ACTION TO THE APPROPRIATE BUTTON IN STORYBOARD--ONCE YOU 'HAVE' STORYBOARD
+
+- (IBAction)onAddImage:(id)sender
+{
+    [self presentViewController:self.imagePicker animated:YES completion:nil];
+}
+
+
+@end
