859da2ae9ec60346446075a355dcf8a97368d6f6
Glen Ruhl <glen.ruhl.90@gmail.com>
2014-08-19 17:28:50 -0500

    that’s it for the day(time)

---
+++ EditProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditProfileViewController.h
+//  INSTAGRAM
+//
+//  Created by Glen Ruhl on 8/19/14.
+//  Copyright (c) 2014 MM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditProfileViewController : UIViewController
+
+@end
---
+++ EditProfileViewController.m
@@ -0,0 +1,23 @@
+//
+//  EditProfileViewController.m
+//  INSTAGRAM
+//
+//  Created by Glen Ruhl on 8/19/14.
+//  Copyright (c) 2014 MM. All rights reserved.
+//
+
+#import "EditProfileViewController.h"
+
+@interface EditProfileViewController ()
+
+@end
+
+@implementation EditProfileViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
