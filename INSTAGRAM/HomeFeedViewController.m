a7f15ea6a09c966d75946da40117d72a65e55e5a
Patrick Hogan <phogan@beatsmusic.com>
2014-08-20 09:32:01 -0500

    made query on home screen, connected tableview outlet

--- INSTAGRAM/HomeFeedViewController.m
+++ INSTAGRAM/HomeFeedViewController.m
@@ -10,6 +10,7 @@
 
 @interface HomeFeedViewController () <UITableViewDataSource, UITableViewDelegate>
 @property NSArray *posts;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -18,6 +19,26 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    PFUser *currentUser = [PFUser currentUser];
+    NSLog(@"Current user: %@", currentUser.username);
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    PFQuery *query = [PFQuery queryWithClassName:@"Post"];
+    [query whereKey:@"recipientIds" equalTo:[[PFUser currentUser] objectId]];
+    [query orderByDescending:@"createdAt"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if (error) {
+            NSLog(@"Error: %@ %@", error, [error userInfo]);
+        }
+        else {
+            self.posts = objects;
+            [self.tableView reloadData];
+            NSLog(@"Retrieved %lu posts", (unsigned long)[self.posts count]);
+        }
+    }];
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
