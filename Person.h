0a66542d50009fb135ecd96026567ab7170cbc4b
Glen Ruhl <glen.ruhl.90@gmail.com>
2014-08-24 10:12:53 -0500

    lets-see-it

---
+++ Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  INSTAGRAM
+//
+//  Created by Glen Ruhl on 8/22/14.
+//  Copyright (c) 2014 MM. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface Person : PFObject
+
+@end
---
+++ Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  INSTAGRAM
+//
+//  Created by Glen Ruhl on 8/22/14.
+//  Copyright (c) 2014 MM. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
