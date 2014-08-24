5308671400db0be19e34bcb01d0133d3dddb16f6
Glen Ruhl <glen.ruhl.90@gmail.com>
2014-08-24 10:48:05 -0500

    getting it clean and ready

--- INSTAGRAM/HomeFeedViewController.m
+++ INSTAGRAM/HomeFeedViewController.m
@@ -34,7 +34,7 @@
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 4;
+    return 2;
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
